d3fd4bc7d9bf97ef335b8118ae7d16ed4ee5f43c
hzmsalah <hzmsalah2@gmail.com>
2023-12-13 13:10:12 +0200

    ambulance main component done

--- src/app/Views/ambulance-manager/ambulance-main/ambulance-main.component.ts
+++ src/app/Views/ambulance-manager/ambulance-main/ambulance-main.component.ts
@@ -1,4 +1,14 @@
 import { Component } from '@angular/core';
+export interface AmbulanceRequestTable {
+  patientName: string;
+  timeOfRequest: string;
+  viewCase: any;
+}
+
+const ELEMENT_DATA: AmbulanceRequestTable[] = [
+  {patientName: 'Mohamed', timeOfRequest:'5:55', viewCase: null},
+  {patientName: 'Ali', timeOfRequest:'7:44', viewCase: null},
+];
 
 @Component({
   selector: 'app-ambulance-main',
@@ -6,5 +16,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./ambulance-main.component.css']
 })
 export class AmbulanceMainComponent {
+  displayedColumns: string[] = ['patientName', 'timeOfRequest', 'viewCase'];
+  dataSource = ELEMENT_DATA;
+  showCase=false;
 
+  viewCase(caseID:any){
+    this.showCase=true;
+  }
 }
