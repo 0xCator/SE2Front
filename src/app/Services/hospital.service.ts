8aab1db75a8250f2e446370a8e8cca36de90a5c0
Nexusrex <ahmedtahoun55@gmail.com>
2023-12-14 19:32:23 +0200

    Basic map stuff

--- src/app/Services/hospital.service.ts
+++ src/app/Services/hospital.service.ts
@@ -1,15 +1,20 @@
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, throttleTime } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
 import { Hospital } from '../Models/hospital.model';
+import { BehaviorSubject } from 'rxjs';
 
 const BASEURL = 'http://localhost:3000/api/hospitals';
 @Injectable({
   providedIn: 'root'
 })
 export class HospitalService {
+  private hospitalsSubject: BehaviorSubject<Hospital[]> = new BehaviorSubject<Hospital[]>([]);
+  public hospitals$: Observable<Hospital[]> = this.hospitalsSubject.asObservable();
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {
+    this.fetchHospitalData();
+  }
 
   getAllHospitals(): Observable<Hospital[]> {
     return this.http.get<Hospital[]>(BASEURL);
@@ -18,9 +23,18 @@ export class HospitalService {
     return this.http.get<Hospital>(BASEURL+"/"+hospitalID);
   }
   createHospital(hospitalData: any){
-    return this.http.post<Hospital>(BASEURL, hospitalData);
+    //this.hospital.next(hospitalData);
+   return this.http.post<Hospital>(BASEURL, hospitalData);
   }
   deleteHospital(hospitalID:any){
     return this.http.delete<Hospital>(BASEURL+"/"+hospitalID);
+  }
+
+  fetchHospitalData() {
+    this.http.get<Hospital[]>(BASEURL).subscribe((data) => {
+      if (this.hospitalsSubject.value.length !== data.length){
+        this.hospitalsSubject.next(data);
+      }
+    });
   }
 }
--- src/app/Views/admin/admin-main/admin-main.component.ts
+++ src/app/Views/admin/admin-main/admin-main.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { timer, Observable, mergeMap, share, switchMap, Subscription, distinctUntilChanged, debounceTime, throttleTime, interval, map } from 'rxjs';
+import { Hospital } from 'src/app/Models/hospital.model';
+import { HospitalService } from 'src/app/Services/hospital.service';
 
 @Component({
   selector: 'app-admin-main',
   templateUrl: './admin-main.component.html',
   styleUrls: ['./admin-main.component.css']
 })
-export class AdminMainComponent {
+export class AdminMainComponent implements OnInit{
+  mapOptions: google.maps.MapOptions = {
+    mapTypeControl: false, 
+    streetViewControl: false,
+ }
+ private hospitalsSubscription!: Subscription;
 
+ hospitalList?: Hospital[];
+ constructor(private hospitalService: HospitalService) {}
+
+ ngOnInit(): void {
+  this.hospitalsSubscription = this.hospitalService.hospitals$.subscribe((data) => {
+    this.hospitalList = data;
+    console.log(this.hospitalList);
+  });
+  timer(0,500).subscribe(
+    (data)=>{
+      this.hospitalService.fetchHospitalData();
+    }
+  )
+ }
+
+ ngOnDestroy() {
+  if (this.hospitalsSubscription) {
+    this.hospitalsSubscription.unsubscribe();
+  }
+}
 }
